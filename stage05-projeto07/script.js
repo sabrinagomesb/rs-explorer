ff875f68b21e5a672e5042b4b6d9eddd5ca0ac51
Sabrina Gomes <sabrinna.gomes.91@gmail.com>
2023-01-05 12:16:29 -0300

    stage 5: add primeiro click event no projeto focus timer

---
+++ stage05-projeto07/script.js
@@ -0,0 +1,12 @@
+let play = document.querySelector('.play')
+let pause = document.querySelector('.pause')
+
+play.addEventListener('click', function(){
+  play.classList.add('hide')
+  pause.classList.remove('hide')
+})
+
+pause.addEventListener('click', function(){
+  pause.classList.add('hide')
+  play.classList.remove('hide')
+})
