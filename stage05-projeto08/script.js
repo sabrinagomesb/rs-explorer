4cda0f039b6976e76a140359ab05331126f850c9
Sabrina Gomes <sabrinna.gomes.91@gmail.com>
2023-01-14 21:48:57 -0300

    adiciona modal estilizado e funcional

--- stage05-projeto08/script.js
+++ stage05-projeto08/script.js
@@ -1,30 +1,58 @@
-let weight = document.querySelector("#weight")
-let height = document.querySelector("#height")
-let btn = document.querySelector(".btn")
-let result = document.querySelector(".result")
+const form = document.querySelector("form")
+const inputWeight = document.querySelector("#weight")
+const inputHeight = document.querySelector("#height")
+const result = document.querySelector(".result")
 
-btn.addEventListener("click", resultIMC)
+const alertError = document.querySelector(".alert-error")
 
+const Modal = {
+  wrapper: document.querySelector(".modal-wrapper"),
+  message: document.querySelector(".modal .title span"),
+  buttonClose: document.querySelector(".modal button.close"),
 
-function resultIMC(event) {
+  open() {
+    Modal.wrapper.classList.add('open')
+  },
+  
+  close() {
+    Modal.wrapper.classList.remove('open')
+  }
+}
+
+form.onsubmit = event => {
   event.preventDefault()
   
   const weightValue = Number(weight.value)
   const heightValue = Number(height.value)
   
   if (weightValue <= 0 || heightValue <= 0) {
-    alert("[ERRO!] Insira os dados corretamente (apenas números).")
+    alertError.classList.add('open')
     return null;
   }
   
   const imc = weightValue / (heightValue / 100) ** 2
-  result.innerHTML = `IMC = ${imc.toFixed(2)}`
-  result.classList.remove("hide")
+  Modal.message.textContent = `Seu IMC é de ${imc.toFixed(2)}`
+  Modal.open()
+  
+  closeModal()
   
   resetValues()
+}
+
+
+function resultIMC() {
+  
+}
+
+function closeModal() {
+  Modal.buttonClose.addEventListener("click", function () {
+    Modal.close()
+  })
 }
 
 function resetValues() {
    weight.value = ""
    height.value = ""
-} 
+   alertError.classList.remove('open')
+}
+
