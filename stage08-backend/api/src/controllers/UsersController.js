a2c462971c371e026c65fd8f7202182b77d79743
sabrinagomesb <sabrinna.gomes.91@gmail.com>
2023-02-25 17:17:12 -0300

    adiciona update de nome e email

--- stage08-backend/api/src/controllers/UsersController.js
+++ stage08-backend/api/src/controllers/UsersController.js
@@ -7,15 +7,64 @@
 
 const AppError = require("../utils/AppError")
 
+const sqliteConnection = require("../database/sqlite")
+
 class UsersController {
-  create(request, response) {
+  async create(request, response) {
     const { name, email, password } = request.body
 
-    if(!name) {
-      throw new AppError("Nome obrigatório")
+    const database = await sqliteConnection()
+    const checkUserExistis = await database.get(
+      "SELECT * FROM users WHERE email = (?)",
+      [email]
+    )
+
+    if (checkUserExistis) {
+      throw new AppError("Este e-mail já está em uso.")
     }
 
-    response.status(201).json({name, email, password})
+    await database.run(
+      "INSERT INTO users (name, email, password) VALUES  (?, ?, ?)",
+      [name, email, password]
+    )
+
+    return response.status(201).json()
+  }
+
+  async update(request, response) {
+    const { name, email } = request.body
+    const { id } = request.params
+
+    const database = await sqliteConnection()
+    const user = await database.get("SELECT * FROM users WHERE id = (?)", [id])
+
+    if (!user) {
+      throw new AppError("Usuário não encontrado")
+    }
+
+    const userWithUpdatedEmail = await database.get(
+      "SELECT * FROM users WHERE email = (?)",
+      [email]
+    )
+
+    if (userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
+      throw new AppError("Este e-mail já está em uso.")
+    }
+
+    user.name = name
+    user.email = email
+
+    await database.run(
+      `
+      UPDATE users SET
+      name = ?,
+      email = ?,
+      update_at = ?
+      WHERE id = ?`,
+      [user.name, user.email, new Date(), id]
+    )
+
+    return response.json()
   }
 }
 
--- stage08-backend/api/src/routes/users.routes.js
+++ stage08-backend/api/src/routes/users.routes.js
@@ -4,17 +4,19 @@ const UsersController = require("../controllers/UsersController")
 
 const usersRoutes = Router() // inicializa o Router que foi requerido na linha1
 
-function myMiddleware(request, response, next) {
-  console.log("Você passou pelo Middleware")
-  if(!request.body.isAdmin) {
-    return response.json( { message: "user unauthorized"})
-  }
-  next()
-}
+// function myMiddleware(request, response, next) {
+//   console.log("Você passou pelo Middleware")
+//   if(!request.body.isAdmin) {
+//     return response.json( { message: "user unauthorized"})
+//   }
+//   next()
+// }
 
 const userController = new UsersController()
 
 // usersRoutes.use(myMiddleware) // → aplica middleware para todas as rotas de usuário.
-usersRoutes.post("/", myMiddleware, userController.create) // add middleware em uma rota específica.
+usersRoutes.post("/",userController.create) // add middleware em uma rota específica.
+usersRoutes.put("/:id", userController.update)
+
 
 module.exports = usersRoutes; // exportação
